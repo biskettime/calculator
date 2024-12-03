6df556fe8e380dbdef305b1f873dc60e976d90bb
biskettime <biskettime>
2024-12-03 15:39:07 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function calculate() {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const operation = document.getElementById("operation").value;
+  let result;
+
+  if (operation === "add") {
+    result = num1 + num2;
+  } else if (operation === "subtract") {
+    result = num1 - num2;
+  } else if (operation === "multiply") {
+    result = num1 * num2;
+  } else if (operation === "divide") {
+    if (num2 === 0) {
+      alert("Error");
+      return;
+    }
+
+    result = num1 / num2;
+  } else {
+    result = "Invalid operation!";
+  }
+
+  document.getElementById("result").textContent = "Result: " + result;
+}
